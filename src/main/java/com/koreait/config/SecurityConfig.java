31abbccbfa90bfb938320b1835aa74d8e40f5e53
jeoninho623 <0623inho@naver.com>
2023-11-14 16:46:57 +0900

    로그아웃, 접근통제 구현

--- src/main/java/com/koreait/config/SecurityConfig.java
+++ src/main/java/com/koreait/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.koreait.config;
 
 import com.koreait.models.member.LoginFailureHandler;
 import com.koreait.models.member.LoginSuccessHandler;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +23,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+        /* 인증설정 - 로그인 S */
         http.formLogin(f -> {
             f.loginPage("/member/login")
                     .usernameParameter("email")
@@ -36,9 +37,32 @@ public class SecurityConfig {
                     .logoutSuccessUrl("/member/login");
         });
 
+        /* 인증설정 - 로그인 S */
+
         http.headers(c -> {
            c.frameOptions(o -> o.sameOrigin());
         });
+
+        /* 인가 설정 - 접근 통제 S */
+        http.authorizeHttpRequests(c -> {
+           c.requestMatchers("/mypage/**").authenticated()  // 회원전용 페이지(로그인한 회원만 접근 가능)
+                   .requestMatchers("/admin/**").hasAuthority("ADMIN")  // 관리자 권한 전용  관리자 권한만 접근 가능
+                   .anyRequest().permitAll();  // 나머지 페이지는 권한 필요 X
+        });
+
+        http.exceptionHandling(c -> {
+           c.authenticationEntryPoint((req,resp,e) -> {
+               String URI = req.getRequestURI();
+               if(URI.indexOf("/admin") != -1) {        // 관리자 페이지 - 401 응답 코드
+                    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "NOT AUTHORIZED");
+               } else {     // 회원 전용 페이지( 예 - /mypage ) -> 로그인 페이지 이동
+                    String url = req.getContextPath() + "/member/login";
+                    resp.sendRedirect(url);
+               }
+           });
+        });
+
+        /* 인가 설정 - 접근 통제 E*/
 
         return http.build();
     }
