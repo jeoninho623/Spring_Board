e206d829284ca1bbc8fa6b540a7879bd5c80f776
Shinaeji <a_to_z_az@naver.com>
2023-11-17 18:18:07 +0900

    연관관계 매핑

--- src/test/java/com/koreait/jpaex/Ex01.java
+++ src/test/java/com/koreait/jpaex/Ex01.java
@@ -1,7 +1,12 @@
 package com.koreait.jpaex;
 
+import com.koreait.commons.constants.MemberType;
+import com.koreait.entities.BoardData;
+import com.koreait.entities.Member;
 import com.koreait.repositories.BoardDataRepository;
 import com.koreait.repositories.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -14,4 +19,32 @@ public class Ex01 {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+        Member member = Member.builder()
+                .email("user01@test.org")
+                .password("123456")
+                .userNm("사용자01")
+                .mtype(MemberType.USER)
+                .build();
+        memberRepository.saveAndFlush(member);
+
+        BoardData item = BoardData.builder()
+                .title("제목")
+                .content("내용")
+                .member(member)
+                .build();
+        boardDataRepository.saveAndFlush(item);
+
+    }
+
+    @Test
+    void test1() {
+        BoardData data = boardDataRepository.findById(1L).orElse(null);
+
+        Member member = data.getMember();
+        String email = member.getEmail(); // 2차 쿼리 실행
+        System.out.println(email);
+    }
 }
